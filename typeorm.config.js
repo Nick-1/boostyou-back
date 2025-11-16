9fee7edd9eaf2d48effdcd51146d30662cc0d9b5
Nikita Pastuhov <n.pastuhov@fxpro.com>
2025-11-16 11:52:49 +0200

    Manage migration

---
+++ typeorm.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { DataSource } = require('typeorm');
+
+module.exports = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+
+  synchronize: false,
+});
--- typeorm.config.ts
+++
@@ -1,9 +0,0 @@
-import { DataSource } from 'typeorm';
-import { databaseConfig } from './src/config/database.config';
-
-export default new DataSource({
-  ...databaseConfig,
-  entities: ['src/**/*.entity.{ts,js}'],
-  migrations: ['src/migrations/*.{ts,js}'],
-  synchronize: false,
-});
